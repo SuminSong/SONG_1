ddb90810efbccbae7ed45510564bef83d622668f
SuminSong <s0611sm@gmail.com>
2017-07-09 12:21:34 +0900

    js

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//var weaponsList = ["AK-47","Bazooka","Nuclear"];
+//
+//var	soldier = {
+//	name:"SM",
+//	gender:"M",
+//	korea:true,
+//	weapon:weaponsList[2]
+//
+//}
+
+//}function add(one, two){
+//	return one+two;
+//}
+//function substract(love, hate){
+//	return love-hate;
+//}
+//function divide(you,i){
+//	return you/i;
+
+var age = prompt("what is your age?");
+
+if(age<18){
+	console.log("Go Home!");
+} else if(age>18 && age<39){
+	console.log("Bottom up!");
+} else if(age>39 && age<59){
+	console.log("Opps!");
+} else{
+	console.log("Die!");
+}
